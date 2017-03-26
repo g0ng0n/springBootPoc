ba482d23d50a72ef0f97128aaef0fe3ecb4fed87
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-03-26 17:29:02 -0300

    added the Employee DAO Implementation

---
+++ src/main/java/com/example/rest/DAO/implementation/EmployeeDAOImpl.java
@@ -0,0 +1,120 @@
+package com.example.rest.DAO.implementation;
+
+import com.example.rest.DAO.EmployeeDAO;
+import com.example.rest.model.Employee;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * Created by gonzalo.gisbert on 26/03/17.
+ */
+public class EmployeeListDAO implements EmployeeDAO {
+
+    private final CopyOnWriteArrayList<Employee> eList = MockEmployeeList.getInstance();
+
+    @Override
+    public List<Employee> getAllEmployees(){
+        return eList;
+    }
+
+
+    @Override
+    public Employee getEmployee(long id){
+        Employee match = null;
+
+        match = eList.stream()
+                .filter(e -> e.getId() == id)
+                .findFirst().orElse(match);
+
+        return match;
+    }
+
+
+    @Override
+    public List<Employee> getByLastName(String name){
+
+        List<Employee> matchList =
+                eList.stream()
+                        .filter((e) -> (e.getLastName().contains(name)))
+                        .collect(Collectors.toList());
+
+        return matchList;
+    }
+
+
+    @Override
+    public List<Employee> getByTitle(String title){
+        List<Employee> matchList =
+                eList.stream()
+                        .filter((e) -> (e.getTitle().contains(title)))
+                        .collect(Collectors.toList());
+
+        return matchList;
+    }
+
+
+    @Override
+    public List<Employee> getByDept(String dept){
+        List<Employee> matchList =
+                eList.stream()
+                        .filter((e) -> (e.getDept().contains(dept)))
+                        .collect(Collectors.toList());
+
+        return matchList;
+    }
+
+
+    @Override
+    public boolean add(Employee employee){
+        long next = eList.size() + 100;
+
+        Employee nextEmployee =
+                new Employee( next, employee.getFirstName(), employee.getLastName(),
+                        employee.getEmail(), employee.getPhone(),
+                        employee.getBirthDate(), employee.getTitle(), employee.getDept());
+
+        eList.add(nextEmployee);
+        return true;
+    }
+
+
+    @Override
+    public boolean update(long id, Employee employee){
+        int matchIndex = -1;
+
+        matchIndex = eList.stream()
+                .filter(e -> e.getId() == id)
+                .findFirst()
+                .map(e -> eList.indexOf(e))
+                .orElse(matchIndex);
+
+        if (matchIndex > -1){
+            eList.set(matchIndex, employee);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+    @Override
+    public boolean delete(long id){
+        int matchIndex = -1;
+
+        matchIndex = eList.stream()
+                .filter(e -> e.getId() == id)
+                .findFirst()
+                .map(e -> eList.indexOf(e))
+                .orElse(matchIndex);
+
+        if (matchIndex > -1){
+            eList.remove(matchIndex);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
